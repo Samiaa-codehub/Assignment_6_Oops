02ecfbd9cf24d97dce53bb565656199a36becac5
Samia <samiaadnan321@gmail.com>
2025-05-06 21:33:30 +0500

    Add files via upload

---
+++ Assignmnet_21/main.py
@@ -0,0 +1,20 @@
+##21. Make a Custom Class Iterable..
+
+##Create a class Countdown that takes a start number. Implement __iter__() and __next__() to make the object iterable in a for-loop, counting down to 0.
+class Countdown:
+    def __init__(self,start):
+        self.start = start
+        self.current = start
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current < 0:
+            raise StopIteration
+        else:
+            current = self.current
+            self.current -= 1
+            return self.current + 1
+coountdown = Countdown(5)
+for number in coountdown:
+    print(number)
+
