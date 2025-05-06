77fbb37f171225ced37c48b190c218a762e3ef30
Samia <samiaadnan321@gmail.com>
2025-05-06 21:47:03 +0500

    Add files via upload

---
+++ Assignment_10/main.py
@@ -0,0 +1,11 @@
+## Instance Method..
+## Create a class Dog with instance variables name and breed. Add an instance method bark() that prints a message including the dog's name.
+
+class Dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed = breed
+    def bark(self):
+        print(f"{self.name} says woof!")
+my_dog = Dog("Buddy","Golden Retriever")
+my_dog.bark()
